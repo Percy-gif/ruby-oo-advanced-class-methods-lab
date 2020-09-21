2eeeab8af33199405c86a864697f14b0c9292766
Percy-gif <percymajor@gmail.com>
2020-09-20 19:04:56 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
@@ -9,5 +9,18 @@ class Song
   def save
     self.class.all << self
   end
+#-----------------
+ def self.create #instantiates and saves the song, and it returns the new song that was created 
+    song = Song.new
+    song.save
+    song 
+ end 
 
+ def self.new_by_name #instantiates a song with a name property 
+    song = Song.new(song_name)
+    # song = Song.new 
+    # song.name = song_name
+ end 
+ 
 end
+
